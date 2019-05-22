716e82fe125ffde9d3679d17a93cb43c19fa5878
Joe Pell <jpell3130@gmail.com>
2019-05-22 10:27:41 -0400

    added controller test for user creation

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+RSpec.describe UsersController, type: :controller do
+  describe "GET #new" do
+    it "renders the :new template" do
+      get :new, {}
+      expect(response).to render_template(:new)
+    end
+  end
+
+  describe "POST #create" do
+    context "with invalid params" do
+      it "re-renders the new template with error" do
+        post :create, params: { user: { username: "joep" } }
+        expect(response).to render_template(:new)
+        expect(response).to have_content("user.error")
+      end
+    end
+
+    context "with valid params" do
+      it "redirects to the root page" do
+        post :create, params: { user: { username: "joep", password: "joespassword" } }
+        expect(response).to redirect_to(:root)
+      end
+    end
+  end
+end
