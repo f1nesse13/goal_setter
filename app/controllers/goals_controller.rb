6ecec0465422b1221ee37ad06860e3e335f183cf
Joe Pell <jpell3130@gmail.com>
2019-05-23 18:11:43 -0400

    added goal views - small fixes to specs and goal controller

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,2 +1,67 @@
 class GoalsController < ApplicationController
+  before_action :redirect_if_logged_out
+
+  def index
+    @goals = current_user.goals
+    render :index
+  end
+
+  def show
+    @goal = Goal.find(params[:id])
+
+    if @goal["public"] == true || @goal.user_id == current_user.id
+      render :show
+    else
+      flash[:errors] = ["This goal is marked private"]
+      redirect_to goals_url
+    end
+  end
+
+  def new
+    @goal = Goal.new()
+    render :new
+  end
+
+  def create
+    @goal = Goal.new(goal_params)
+    @goal.user_id = current_user.id
+    if @goal.save
+      flash[:notice] = ["Goal successfully created"]
+      redirect_to goal_url(@goal)
+    else
+      flash[:errors] = @goal.errors.full_messages
+      render :new
+    end
+  end
+
+  def update
+    @goal = Goal.find(params[:id])
+    if @goal.user_id == current_user.id
+      @goal.update(completed: !@goal.completed)
+      redirect_to goals_url
+    else
+      flash[:errors] = ["You can't complete other users goals"]
+      redirect_back
+    end
+  end
+
+  def destroy
+    @goal = Goal.find(params[:id])
+
+    if @goal.user_id == current_user.id
+      @goal.destroy
+      redirect_to goals_url
+    else
+      flash[:errors] = ["You can't delete other users goals"]
+      redirect_to goals_url
+    end
+  end
+
+  private
+
+  def goal_params
+    params[:goal][:public] = params[:goal][:public] == "private" ? false : true
+    params[:goal][:completed] = params[:goal][:completed] == "complete" ? true : false
+    params.require(:goal).permit(:title, :details, :public, :completed)
+  end
 end
--- spec/features/goals_spec.rb
+++ spec/features/goals_spec.rb
@@ -3,47 +3,44 @@ require "rails_helper"
 
 feature "creating a goal" do
   scenario "has a new goal page" do
-    visit new_goal_url
-    expect(page).to have_content("New Goal")
-  end
-
-  scenario "redirects to goal after a goal is created" do
-    visit new_goal_url
-    fill_in "Title", :with => "Generic Goal"
-    fill_in "Description", :with => "Generic description"
-    find("form input[type=submit]").click
-    expect(page).to have_text("Goal successfully created")
-  end
-end
-
-feature "viewing created goals" do
-  before(:each) do
-    visit new_user_url
-    fill_in "Username", :with => "joep"
-    fill_in "Password", :with => "joespassword"
-    click_on "Create Account"
-  end
-  scenario "view a public goal created by another user" do
-    visit new_goal_url
-    fill_in "Title", :with => "Generic Goal"
-    fill_in "Description", :with => "Generic description"
-    check("Public")
-    find("form input[type=submit]").click
-    click_on "Logout"
     visit new_user_url
     fill_in "Username", :with => "laylap"
     fill_in "Password", :with => "laylaspassword"
+    click_on "Create Account"
+    visit new_goal_url
+    expect(page).to have_text("Add a new goal")
+  end
+end
+
+feature "view created goals" do
+  scenario "public goal created by another user" do
+    visit new_user_url
+    fill_in "Username", with: "joep"
+    fill_in "Password", with: "joespassword"
+    click_on "Create Account"
+    click_button "Create Goal"
+    fill_in "Title", with: "Generic Goal"
+    fill_in "Details", with: "Generic description"
+    find("form .submit").click
+    click_on "Logout"
+    visit new_user_url
+    fill_in "Username", with: "laylap"
+    fill_in "Password", with: "laylaspassword"
     click_on "Create Account"
     visit goal_url(Goal.last.id)
     expect(page).to have_text("Generic Goal")
   end
 
   scenario "unable to view a private goal created by another user" do
+    visit new_user_url
+    fill_in "Username", :with => "joep"
+    fill_in "Password", :with => "joespassword"
+    click_on "Create Account"
     visit new_goal_url
     fill_in "Title", :with => "Generic Goal"
-    fill_in "Description", :with => "Generic description"
-    uncheck("Public")
-    find("form input[type=submit]").click
+    fill_in "Details", :with => "Generic description"
+    check("Private?")
+    find("form .submit").click
     click_on "Logout"
     visit new_user_url
     fill_in "Username", :with => "laylap"
@@ -56,12 +53,16 @@ end
 
 feature "ability to complete and delete a goal" do
   scenario "complete button edits completed status" do
+    visit new_user_url
+    fill_in "Username", :with => "laylap"
+    fill_in "Password", :with => "laylaspassword"
+    click_on "Create Account"
     visit new_goal_url
     fill_in "Title", :with => "Generic Goal"
-    fill_in "Description", :with => "Generic description"
-    find("form input[type=submit]").click
-    click_link("Complete")
-    find_link("Have not completed").visible?
+    fill_in "Details", :with => "Generic description"
+    find("form .submit").click
+    click_on "Mark as complete"
+    expect(page).to have_button("Mark as incomplete")
   end
 
   feature "ability to delete a goal" do
@@ -72,9 +73,10 @@ feature "ability to complete and delete a goal" do
       click_on "Create Account"
       visit new_goal_url
       fill_in "Title", :with => "Generic Goal"
-      fill_in "Description", :with => "Generic description"
-      find("form input[type=submit]").click
+      fill_in "Details", :with => "Generic description"
+      find("form .submit").click
       click_button("Delete this goal")
+      expect(page).not_to have_content("Generic Goal")
     end
   end
 end
